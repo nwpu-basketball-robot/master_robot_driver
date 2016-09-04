b9f69d72eff7bdd2760be3810ba537181d8d1ac8
huyanxin <425707140@qq.com>
2016-09-04 13:45:49 +0800

    添加最小速度限制

--- src/robot_driver.cpp
+++ src/robot_driver.cpp
@@ -21,12 +21,13 @@
 /*
 *		2016-7-9 update
 *add func : move in robotic  coordinate system
-*add the topic "/cmd_move_robot"  
+*add the topic "/cmd_move_robot"
 */
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <basketball_msgs/robot_message.h>
-
+#include <math.h>
+#define min_speed 0.07
 class RobotDriver
 {
 public:
@@ -39,14 +40,14 @@ private:
     double publish_rate_ ;
     double wheel_center_ ;
     double wheel_radius_ ;
-    uint8_t base_cmd_id_ ; 
+    uint8_t base_cmd_id_ ;
     ros::Subscriber cmd_vel_world_sub_ ;
     ros::Subscriber cmd_vel_robot_sub_ ;
     ros::Publisher robot_message_pub_ ;
 private:
     //消息回调函数
     void cmdWorldMoveCallBack(const geometry_msgs::TwistConstPtr &ptr) ; //处理全局坐标系下速度的回调函数
-    void cmdRobotMoveCallBack(const geometry_msgs::TwistConstPtr &ptr) ; //处理机器人坐标系下速度回调函数 
+    void cmdRobotMoveCallBack(const geometry_msgs::TwistConstPtr &ptr) ; //处理机器人坐标系下速度回调函数
     //速度发布接口
     void pubBaseCmd(const uint8_t func, const double move_x, const double move_y, const double speed_w) ;
     //急停接口
@@ -70,41 +71,63 @@ RobotDriver::RobotDriver(ros::NodeHandle node)
 
 RobotDriver::~RobotDriver()
 {
-    brake() ; 
+    brake() ;
     nh_.shutdown();
 }
 
 void RobotDriver::brake()
 {
-	pubBaseCmd(0x01 , 0 , 0 , 0) ; 
+	pubBaseCmd(0x01 , 0 , 0 , 0) ;
 }
 
 void RobotDriver::pubBaseCmd(const uint8_t func, const double move_x, const double move_y, const double speed_w)
 {
-	basketball_msgs::robot_message robot_cmd_msg ; 
-	robot_cmd_msg.data.resize(18 , 0) ; 
-	uint8_t *data_ptr = robot_cmd_msg.data.data() ; 
-	int data_len = 13 ; 
-	data_ptr[0] = data_ptr[1] = 0xff ; 
-	data_ptr[2] = base_cmd_id_ ; 
+	basketball_msgs::robot_message robot_cmd_msg ;
+	robot_cmd_msg.data.resize(18 , 0) ;
+	uint8_t *data_ptr = robot_cmd_msg.data.data() ;
+	int data_len = 13 ;
+	data_ptr[0] = data_ptr[1] = 0xff ;
+	data_ptr[2] = base_cmd_id_ ;
 	data_ptr[3] = (u_int8_t)(data_len>>8) ;
    	data_ptr[4] = (u_int8_t)(data_len & 0xff) ;
    	data_ptr[5] = func ;
 	*(float *)(data_ptr+6)  = move_x;
    	*(float *)(data_ptr+10)  = move_y;
    	*(float *)(data_ptr+14)  = speed_w;
-	robot_message_pub_.publish(robot_cmd_msg) ; 
-}	
+	robot_message_pub_.publish(robot_cmd_msg) ;
+}
 
 void RobotDriver::cmdWorldMoveCallBack(const geometry_msgs::TwistConstPtr &ptr)
 {
-    ROS_INFO("send cmd world info\n") ; 
-    pubBaseCmd(0x01,ptr->linear.x , ptr->linear.y,ptr->angular.z);
+    ROS_INFO("send cmd world info\n") ;
+    double x =0.0,y = 0.0;
+    x = ptr->linear.x;
+    y = ptr->linear.y;
+    double s = sqrt(x*x+y*y);
+    //速度的小于0.038底盘自锁
+    if(s != 0.0 && s < min_speed )
+    {
+      x = min_speed*x/s;
+      y = min_speed*y/s;
+    }
+    pubBaseCmd(0x01,x , y,ptr->angular.z);
+  //  pubBaseCmd(0x01,ptr->linear.x , ptr->linear.y,ptr->angular.z);
 }
 void RobotDriver::cmdRobotMoveCallBack(const geometry_msgs::TwistConstPtr &ptr)
 {
-    ROS_INFO("send cmd robot info\n") ; 
-    pubBaseCmd(0x05,ptr->linear.x , ptr->linear.y,ptr->angular.z);
+    ROS_INFO("send cmd robot info\n") ;
+    //速度的小于0.038底盘自锁
+    double x =0.0,y = 0.0;
+    x = ptr->linear.x;
+    y = ptr->linear.y;
+    double s = sqrt(x*x+y*y);
+    if(s !=0.0 && s < min_speed)
+    {
+      x = min_speed*x/s;
+      y = min_speed*y/s;
+    }
+    pubBaseCmd(0x05,x , y,ptr->angular.z);
+  //  pubBaseCmd(0x05,ptr->linear.x , ptr->linear.y,ptr->angular.z);
 }
 
 int main(int argc , char **argv)
